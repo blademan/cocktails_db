5f00b7aa75346ad34b907823637b1c07873a5442
Eduard <blademan82@gmail.com>
2022-08-14 00:52:33 +0300

    App structure created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 function App() {
- return <>app</>;
+ return (
+  <Router>
+   <Routes>
+    <Route path="/" element={<Home />} />
+   </Routes>
+  </Router>
+ );
 }
 
 export default App;
---
+++ src/components/Cocktail.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Cocktail() {
+ return <div>Cocktail</div>;
+}
